2176e463f7c2e26810642fd695d29362528a47ef
Spolf <51790802+Spolf@users.noreply.github.com>
2020-04-07 18:25:32 -0300

    minhas alterações

--- index.php
+++ index.php
@@ -1,6 +1,73 @@
 <form method="post">
-  <label for="">Nome</label>
+  <label for="">Nome Completo:</label>
   <input type="text" name="nome"><br>
-  <label for="">Sobrenome</label>
-  <input type="text" name="sobrenome">
+  <label for="">Data de Nascimento:</label>
+  <input type="date" name="data"><br>
+  <label for="">CPF:</label>
+  <input type="number" name="CPF"><br>
+  <label for="">Nome do PAI:</label>
+  <input type="text" name="nome"><br>
+  <label for="">Nome da MAE:</label>
+  <input type="text" name="nome"><br>
+  <label for="">Endereço:</label>
+  <input type="text" name="endereco"><br>
+  <label for="">Numero:</label>
+  <input type="number" name="numero"><br>
+  <label for="">Bairro:</label>
+  <input type="text" name="bairro"><br>
+  <label for="">CEP:</label>
+  <input type="number" name="cep"><br>
+  <label for="">Cidade/Estado:</label>
+  <input type="text" name="cidade_estado"><br>
+  <label for="">Telefone:(DDD+Telefone)</label>
+  <input type="number" name="telefone"><br>
+  <label for="">Enviar Curriculo:</label>
+  <input type="file" name="curriculo"><br>
+  <label for="">Email:</label>
+  <input type="email" name="email"><br>
+  <label for="">DADOS ACADEMICOS</label><br>
+  <label for="">Instituição de Ensino:</label>
+  <input type="text" name="instituicao"><br>
+  <label for="">Curso:</label>
+  <input type="text" name="curso"><br>
+  <label for="">Periodo:</label>
+  <input type="text" name="periodo"><br>  
+  <label for="">Turno:</label><br>
+  <input type="radio" id="manha" name="gender" value="manha">
+  <label for="">Manha</label><br>
+  <input type="radio" id="tarde" name="gender" value="tarde">
+  <label for="">Tarde</label><br>
+  <input type="radio" id="noite" name="gender" value="noite">
+  <label for="">Noite</label><br>
+  <label for="">Disponibilidade Estagio: <br></label>
+  <input type="radio" id="manha" name="gender" value="manha">
+  <label for="">Manha</label><br>
+  <input type="radio" id="tarde" name="gender" value="tarde">
+  <label for="">Tarde</label><br>
+  <label for="">Outro</label>
+  <input type="text" name="noite"><br>
+  <label for="">Declaro para os Devidos fins, que disponho de 04 (quatro) horas diárias e 20 (vinte) horas semanais para dedicação exclusiva ao estágio junto ao Poder Executivo do Estado do Piauí, que não o acumularei com outro estágio, mesmo que não remunerado, e que preencho os requisitos legais constantes no Decreto n° 13.840, de 21 de setembro de 2009, para admissão ao estágio não obrigatório remunerado :</label><br>
+  <input type="radio" id="sim" name="gender" value="sim">
+  <label for="">Sim</label><br>
+  <input type="radio" id="nao" name="gender" value="nao">
+  <label for="">Não</label><br>
+  <label for="">Declaro que todas as informações acima prestadas são verdadeiras, e assumo a inteira responsabilidade pelas mesmas :</label><br>
+  <input type="radio" id="sim" name="gender" value="sim">
+  <label for="">Sim</label><br>
+  <input type="radio" id="nao" name="gender" value="nao">
+  <label for="">Não</label><br>
+  <label for="">Comprovante de matricula da instituicao:</label>
+  <input type="file" name="matricula"><br>
+
 </form>
+
+
+
+
+
+
+
+
+
+
+
