0469f496614fbb17fae8cd20d3d13a4925c94cf0
Spolf <51790802+Spolf@users.noreply.github.com>
2020-04-07 19:23:26 -0300

    novo commit

--- index.php
+++ index.php
@@ -33,28 +33,28 @@
   <label for="">Periodo:</label>
   <input type="text" name="periodo"><br>  
   <label for="">Turno:</label><br>
-  <input type="radio" id="manha" name="gender" value="manha">
+  <input type="radio" id="manha" name="turno" value="manha">
   <label for="">Manha</label><br>
-  <input type="radio" id="tarde" name="gender" value="tarde">
+  <input type="radio" id="tarde" name="turno" value="tarde">
   <label for="">Tarde</label><br>
-  <input type="radio" id="noite" name="gender" value="noite">
+  <input type="radio" id="noite" name="turno" value="noite">
   <label for="">Noite</label><br>
   <label for="">Disponibilidade Estagio: <br></label>
-  <input type="radio" id="manha" name="gender" value="manha">
+  <input type="radio" id="manha" name="horario" value="manha">
   <label for="">Manha</label><br>
-  <input type="radio" id="tarde" name="gender" value="tarde">
+  <input type="radio" id="tarde" name="horario" value="tarde">
   <label for="">Tarde</label><br>
   <label for="">Outro</label>
   <input type="text" name="noite"><br>
   <label for="">Declaro para os Devidos fins, que disponho de 04 (quatro) horas diárias e 20 (vinte) horas semanais para dedicação exclusiva ao estágio junto ao Poder Executivo do Estado do Piauí, que não o acumularei com outro estágio, mesmo que não remunerado, e que preencho os requisitos legais constantes no Decreto n° 13.840, de 21 de setembro de 2009, para admissão ao estágio não obrigatório remunerado :</label><br>
-  <input type="radio" id="sim" name="gender" value="sim">
+  <input type="radio" id="sim" name="lei" value="sim">
   <label for="">Sim</label><br>
-  <input type="radio" id="nao" name="gender" value="nao">
+  <input type="radio" id="nao" name="lei" value="nao">
   <label for="">Não</label><br>
   <label for="">Declaro que todas as informações acima prestadas são verdadeiras, e assumo a inteira responsabilidade pelas mesmas :</label><br>
-  <input type="radio" id="sim" name="gender" value="sim">
+  <input type="radio" id="sim" name="acordo" value="sim">
   <label for="">Sim</label><br>
-  <input type="radio" id="nao" name="gender" value="nao">
+  <input type="radio" id="nao" name="acordo" value="nao">
   <label for="">Não</label><br>
   <label for="">Comprovante de matricula da instituicao:</label>
   <input type="file" name="matricula"><br>
